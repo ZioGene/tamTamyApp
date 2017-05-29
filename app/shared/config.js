4d5a4b183937a2e3cec0acf3ec5f1b7ad71f2acc
g.zappulla <g.zappulla@reply.it>
2017-05-29 15:23:42 +0200

    push new version with required dep

--- app/shared/config.js
+++ app/shared/config.js
@@ -8,3 +8,4 @@ var Config = (function () {
 Config.apiUrl = 'https://tamtamy.reply.eu/tamtamy/api/';
 Config.token = '';
 exports.Config = Config;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUE7SUFBQTtJQUdBLENBQUM7SUFBRCxhQUFDO0FBQUQsQ0FBQyxBQUhEO0FBQ1MsYUFBTSxHQUFHLHVDQUF1QyxDQUFDO0FBQ2pELFlBQUssR0FBRyxFQUFFLENBQUM7QUFGUCx3QkFBTSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjbGFzcyBDb25maWcge1xuICBzdGF0aWMgYXBpVXJsID0gJ2h0dHBzOi8vdGFtdGFteS5yZXBseS5ldS90YW10YW15L2FwaS8nO1xuICBzdGF0aWMgdG9rZW4gPSAnJztcbn0iXX0=
--- app/shared/user/user.js
+++ app/shared/user/user.js
@@ -6,3 +6,4 @@ var User = (function () {
     return User;
 }());
 exports.User = User;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInVzZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUFBO0lBR0EsQ0FBQztJQUFELFdBQUM7QUFBRCxDQUFDLEFBSEQsSUFHQztBQUhZLG9CQUFJIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIFVzZXIge1xyXG4gICAgdXNlcmlkOiBzdHJpbmc7XHJcbiAgICBwYXNzd29yZDogc3RyaW5nO1xyXG59Il19
